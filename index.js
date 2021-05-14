752609c9b10afcea3dcbfdb4c5e6c4d721d2ba8b
andrew <neon.duncan@gmail.com>
2021-05-14 07:28:59 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+const dodger = document.querySelector("#dodger")
+let dodgerLeft = styleConverter(dodger.style.left)
+let dodgerRight = styleConverter(dodger.style.left) - 20
+
+function styleConverter(attr) {
+  if (typeof attr === "string") {
+    return parseInt(attr)
+  } else if (typeof attr === "number") {
+    return attr.toString() + "px"
+  }
+}
+
+
+function moveDodgerLeft() {
+  let newPos = styleConverter(dodger.style.left) - 10
+  console.log(styleConverter(newPos))
+  if (newPos > 0) {
+    dodger.style.left = styleConverter(newPos)
+  }
+}
+
+function moveDodgerRight() {
+  let newPos = styleConverter(dodger.style.left) + 10
+  console.log(styleConverter(newPos))
+  if (newPos < 400) {
+    dodger.style.left = styleConverter(newPos)
+  }
+}
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode === 37) {
+    moveDodgerLeft()
+  } else if (e.keyCode === 39) {
+    moveDodgerRight()
+  }
+})
